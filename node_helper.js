027f6520cb9928164cfb873ad121b4da6da89209
Olexandr Savchuk <olexandr.savchuk@maibornwolff.de>
2022-05-17 23:15:36 +0200

    Refactor / minor cleanup in node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -8,16 +8,15 @@
 var NodeHelper = require("node_helper");
 var request = require('request');
 var moment = require('moment');
- 
+
 module.exports = NodeHelper.create({
 
-  start: function() {
+  start: function () {
     console.log("====================== Starting node_helper for module [" + this.name + "]");
   },
-  
-  
+
   // subclass socketNotificationReceived
-  socketNotificationReceived: function(notification, payload){
+  socketNotificationReceived: function (notification, payload) {
     if (notification === 'GOOGLE_TRAFFIC_GET') {
 
       //first data opull after new config
@@ -26,71 +25,54 @@ module.exports = NodeHelper.create({
     }
   },
 
-
-	
-	getPredictions: function(payload) {
-		var self = this;
+  getPredictions: function (payload) {
+    var self = this;
 
     var returned = 0;
     var predictions = new Array();
 
-		payload.destinations.forEach(function(dest, index) {
-			request({url: dest.url, method: 'GET'}, function(error, response, body) {
-				
+    payload.destinations.forEach(function (dest, index) {
+      request({ url: dest.url, method: 'GET' }, function (error, response, body) {
+
         var prediction = new Object({
           config: dest.config
         });
 
-        if(!error && response.statusCode == 200){
+        if (!error && response.statusCode == 200) {
 
-          var data = JSON.parse(body);
+          var apiData = JSON.parse(body);
 
-
-          if (data.error_message) {
-            console.log("MMM-MyCommute: " + data.error_message);
+          if (apiData.error_message) {
+            console.log("MMM-MyCommute: " + apiData.error_message);
             prediction.error = true;
           } else {
-  
             var routeList = new Array();
-            for (var i = 0; i < data.routes.length; i++) {
-              var r = data.routes[i];
-              var routeObj = new Object({
-                summary: r.summary,
-                time: r.legs[0].duration.value
+            for (var i = 0; i < apiData.routes.length; i++) {
+              var apiRoute = apiData.routes[i];
+
+              var routeForFrontend = new Object({
+                summary: apiRoute.summary,
+                time: apiRoute.legs[0].duration.value
               });
 
-              if (r.legs[0].duration_in_traffic) {
-                routeObj.timeInTraffic = r.legs[0].duration_in_traffic.value;
+              if (apiRoute.legs[0].duration_in_traffic) {
+                routeForFrontend.timeInTraffic = apiRoute.legs[0].duration_in_traffic.value;
               }
-              if (r.legs[0].distance) {
-                routeObj.distance = r.legs[0].distance.value;
+              if (apiRoute.legs[0].distance) {
+                routeForFrontend.distance = apiRoute.legs[0].distance.value;
               }
-              if (dest.config.mode && dest.config.mode == 'transit') {
-                var transitInfo = new Array();
-                var gotFirstTransitLeg = false;
-                for (var j = 0; j < r.legs[0].steps.length; j++) {
-                  var s = r.legs[0].steps[j];
-
-                  if (s.transit_details) {
-                    var arrivalTime = '';
-                    if (!gotFirstTransitLeg && dest.config.showNextVehicleDeparture) {
-                      gotFirstTransitLeg = true;
-                      // arrivalTime = ' <span class="transit-arrival-time">(next at ' + s.transit_details.departure_time.text + ')</span>';
-                      arrivalTime = moment(s.transit_details.departure_time.value * 1000);
-                    }
-                    transitInfo.push({routeLabel: s.transit_details.line.short_name ? s.transit_details.line.short_name : s.transit_details.line.name, vehicle: s.transit_details.line.vehicle.type, arrivalTime: arrivalTime});
-                  }
-                  routeObj.transitInfo = transitInfo;
-                }
+              if (dest.config.mode == 'transit') {
+                routeForFrontend.transitInfo = generateTransitInfo(apiRoute, dest);
               }
-              routeList.push(routeObj);
+              
+              routeList.push(routeForFrontend);
             }
             prediction.routes = routeList;
-            
+
           }
 
         } else {
-          console.log( "Error getting traffic prediction: " + response.statusCode );
+          console.log("Error getting traffic prediction: " + response.statusCode);
           prediction.error = true;
 
         }
@@ -98,12 +80,35 @@ module.exports = NodeHelper.create({
         predictions[index] = prediction;
         returned++;
 
-        if (returned == payload.destinations.length) {          
+        if (returned == payload.destinations.length) {
           self.sendSocketNotification('GOOGLE_TRAFFIC_RESPONSE' + payload.instanceId, predictions);
         };
 
       });
     });
-	}
-	
+  }
+
 });
+
+function generateTransitInfo(apiRoute, dest) {
+  var transitInfo = new Array();
+  var gotFirstTransitLeg = false;
+  for (var j = 0; j < apiRoute.legs[0].steps.length; j++) {
+    var apiRouteStep = apiRoute.legs[0].steps[j];
+
+    if (apiRouteStep.transit_details) {
+      var arrivalTime = '';
+      if (!gotFirstTransitLeg && dest.config.showNextVehicleDeparture) {
+        gotFirstTransitLeg = true;
+        // arrivalTime = ' <span class="transit-arrival-time">(next at ' + s.transit_details.departure_time.text + ')</span>';
+        arrivalTime = moment(apiRouteStep.transit_details.departure_time.value * 1000);
+      }
+      transitInfo.push({
+        routeLabel: apiRouteStep.transit_details.line.short_name ? apiRouteStep.transit_details.line.short_name : apiRouteStep.transit_details.line.name,
+        vehicle: apiRouteStep.transit_details.line.vehicle.type,
+        arrivalTime: arrivalTime
+      });
+    }
+  }
+  return transitInfo;
+}
